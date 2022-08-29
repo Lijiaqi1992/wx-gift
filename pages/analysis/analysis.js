b8970da93a66f9e89ef116646b1399c7a4256641
lijiaqi <lijiaqi@tydic.com>
2022-08-29 10:26:14 +0800

    临时提交

--- pages/analysis/analysis.js
+++ pages/analysis/analysis.js
@@ -1,39 +1,68 @@
-// pages/analysis/analysis.js
-Component({
-  options: {
-    addGlobalClass: true,
-  },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+import * as echarts from '../../ec-canvas/echarts';
 
-  },
+const app = getApp();
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    chart: {}
-  },
-  lifetimes: {
-    // 生命周期函数，可以为函数，或一个在 methods 段中定义的方法名
-    attached: function () {
-      this.showChat();
-      wx.setBackgroundColor({
-        backgroundColor: '#111111', // 窗口的背景色为白色
-      })
+function initChart(canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // new
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        backgroundColor: "#ffffff",
+        series: [{
+            label: {
+                normal: {
+                    fontSize: 14
+                }
+            },
+            type: 'pie',
+            center: ['50%', '50%'],
+            radius: ['20%', '40%'],
+            data: [{
+                value: 55,
+                name: '北京'
+            }, {
+                value: 20,
+                name: '武汉'
+            }, {
+                value: 10,
+                name: '杭州'
+            }, {
+                value: 20,
+                name: '广州'
+            }, {
+                value: 38,
+                name: '上海'
+            }]
+        }]
+    };
+
+    chart.setOption(option);
+    console.log(chart);
+    return chart;
+}
+
+Page({
+
+    data: {
+        ec: {
+            onInit: initChart
+        },
+        TabCur: 0,
+        tab: [
+        {
+            name: '收礼',
+            TabCur: 0
+        },
+        {
+            name: '送礼',
+            TabCur: 1
+        },
+        ],
     },
-    moved: function () {},
-    detached: function () {},
-  },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    showChat() {
 
-    }
-  }
-})
+});
